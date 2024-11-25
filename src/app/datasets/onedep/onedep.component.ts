d63854bfbf528fa32c29302f46ca1d8be9b9ef18
sofyalaski <sofyalaskina@gmail.com>
2024-11-25 16:54:21 +0000

    add list of ORCID IDs, token not passed yet

--- src/app/datasets/onedep/onedep.component.ts
+++ src/app/datasets/onedep/onedep.component.ts
@@ -6,6 +6,7 @@ import {
   FormBuilder,
   FormControl,
   FormGroup,
+  FormArray,
   Validators,
 } from "@angular/forms";
 import { Store } from "@ngrx/store";
@@ -31,7 +32,23 @@ export class OneDepComponent implements OnInit {
   methodsList = MethodsList;
   experiment = Experiment;
   selectedFile: { [key: string]: File | null } = {};
+  emFile = EmFile;
+  files = EmFiles;
+  detailsOverflow: string = 'hidden';
 
+
+  @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement> | undefined;
+
+  fileTypes = [
+    { header: 'Main Map', key: this.emFile.MainMap },
+    { header: 'Half Map (1)', key: this.emFile.HalfMap1 },
+    { header: 'Half Map (2)', key: this.emFile.HalfMap2 },
+    { header: 'Mask Map', key: this.emFile.MaskMap },
+    { header: 'Additional Map', key: this.emFile.AddMap },
+    { header: 'Coordinates', key: this.emFile.Coordinates },
+    { header: 'Public Image', key: this.emFile.Image },
+    { header: 'FSC-XML', key: this.emFile.FSC },
+  ];
 
   constructor(public appConfigService: AppConfigService,
     private store: Store,
@@ -56,71 +73,51 @@ export class OneDepComponent implements OnInit {
       associatedMap: new FormControl(false),
       compositeMap: new FormControl(false),
       emdbId: new FormControl(""),
-
-      mainMap: {
-        name: "",
-        type: "vo-map",
-        pathToFile: "",
-        contour: 0.0,
-        details: "",
-      },
-      halfMap1: {
-        name: "",
-        type: "half-map",
-        pathToFile: "",
-        contour: 0.0,
-        details: "",
-      },
-      halfMap2: {
-        name: "",
-        type: "half-map",
-        pathToFile: "",
-        contour: 0.0,
-        details: "",
-      },
-      mask: {
-        name: "",
-        type: "mask-map",
-        pathToFile: "",
-        contour: 0.0,
-        details: "",
-      },
-      addMap: {
-        name: "",
-        type: "add-map",
-        pathToFile: "",
-        contour: 0.0,
-        details: "",
-      },
-      coordinates: {
-        name: "",
-        type: "co-cif",
-        pathToFile: "",
-        details: "",
-      },
-      image: {
-        name: "",
-        type: "img-emdb",
-        pathToFile: "",
-        details: "",
-      },
-      // pathToCif: {  --> should be extracted from this.dataset.scientificMetadata
-      //   name: "",
-      //   type: "undef",
-      //   pathToFile: "",
-      //   details: "",
-      // },
-      fsc: {
-        name: "",
-        type: "fsc-xml",
-        pathToFile: "",
-        details: "",
-      },
+      email: this.user.email,
+      jwtToken: new FormControl(""),
+      orcid: this.fb.array([]),
     })
   }
 
+  get orcidArray(): FormArray {
+    return this.form.get('orcid') as FormArray;
+  }
 
-  onFileSelected(event: Event, controlName: string) {
+  hasUnsavedChanges() {
+    return this._hasUnsavedChanges;
+  }
+  onHasUnsavedChanges($event: boolean) {
+    this._hasUnsavedChanges = $event;
+  }
+  addOrcidField(): void {
+    const orcidField = this.fb.group({
+      orcidId: ['', [Validators.required, Validators.pattern(/^(\d{4}-){3}\d{4}$/)]],
+    });
+    this.orcidArray.push(orcidField);
+  }
+  removeOrcidField(index: number): void {
+    this.orcidArray.removeAt(index);
+  }
+
+  autoGrow(event: Event): void {
+    const textarea = event.target as HTMLTextAreaElement;
+    const lineHeight = parseInt(getComputedStyle(textarea).lineHeight, 10);
+    const maxLines = 5;
+
+    // Reset height to auto to calculate scrollHeight
+    textarea.style.height = 'auto';
+
+    // Set the height based on the scrollHeight but limit it
+    const newHeight = Math.min(textarea.scrollHeight, lineHeight * maxLines);
+    textarea.style.height = `${newHeight}px`;
+
+    // Update overflow property based on height
+    this.detailsOverflow = textarea.scrollHeight > newHeight ? 'auto' : 'hidden';
+  }
+  onChooseFile(fileInput: HTMLInputElement): void {
+    fileInput.click();
+  }
+  onFileSelected(event: Event, controlName: EmFile) {
     const input = event.target as HTMLInputElement;
     console.log(input);
     if (input.files && input.files.length > 0) {
@@ -132,10 +129,25 @@ export class OneDepComponent implements OnInit {
     }
   }
   onDepositClick() {
-    const formData = this.form.value;
-    // need to properly catch the dataset details
-    console.log("creating deposition", formData)
-    this.http.post("http://localhost:8080/onedep", formData).subscribe(
+    const formDataToSend = new FormData();
+    formDataToSend.append('email', this.form.value.email);
+    formDataToSend.append('orcidIds', this.form.value.orcidArray);
+    formDataToSend.append('metadata', JSON.stringify(this.form.value.metadata));
+    formDataToSend.append('experiments', this.form.value.emMethod);
+    // emdbId: this.form.value.emdbId, 
+    var fileMetadata = []
+
+    for (const key in this.files) {
+      if (this.files[key].file) {
+        formDataToSend.append('file', this.files[key].file);
+        fileMetadata.push({ name: this.files[key].name, type: this.files[key].type, contour: this.files[key].contour, details: this.files[key].details });
+      }
+    }
+    formDataToSend.append('fileMetadata', JSON.stringify(fileMetadata));
+
+    this.http.post("http://localhost:8080/onedep", formDataToSend, {
+      headers: {}
+    }).subscribe(
       response => {
         console.log('created deposition in OneDep', response);
       },
