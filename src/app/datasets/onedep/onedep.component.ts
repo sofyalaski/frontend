c7071cd08111eff8d7c2c4d142e9c4dd448ed8de
sofyalaski <sofyalaskina@gmail.com>
2024-12-09 09:15:08 +0000

    ORCID field of same size no matter window size

--- src/app/datasets/onedep/onedep.component.ts
+++ src/app/datasets/onedep/onedep.component.ts
@@ -18,7 +18,7 @@ import {
   selectCurrentUser
 } from "state-management/selectors/user.selectors";
 import { User } from "shared/sdk";
-import { MethodsList, DepositionFiles, EmFile } from "./types/methods.enum"
+import { MethodsList, EmFile, DepositionFiles, DepositionAddMap } from "./types/methods.enum" 
 import { Subscription, fromEvent } from "rxjs";
 
 
@@ -31,19 +31,18 @@ export class OneDepComponent implements OnInit {
   private subscriptions: Subscription[] = [];
   private _hasUnsavedChanges = false;
 
+  
   appConfig = this.appConfigService.getConfig();
   dataset: Dataset | undefined;
   user: User | undefined;
   form: FormGroup;
   showAssociatedMapQuestion: boolean = false;
   methodsList = MethodsList;
-
   selectedFile: { [key: string]: File | null } = {};
-  formSubmitted = false;
   emFile = EmFile;
-  obligatoryFiles = {}; 
+  fileTypes: DepositionFiles[];
   detailsOverflow: string = 'hidden';
-  fileTypes: { [f in EmFile]?: DepositionFiles };
+  additionalMaps = 0;
 
   @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement> | undefined;
 
@@ -79,73 +78,77 @@ export class OneDepComponent implements OnInit {
       email: this.user.email,
       jwtToken: new FormControl(""),
       metadata: this.dataset.scientificMetadata,
-      experiments: this.fb.array([]),
       emMethod: new FormControl(""),
       deposingCoordinates: new FormControl(null, Validators.required),
       associatedMap: new FormControl(null, Validators.required),
       compositeMap: new FormControl(null, Validators.required),
       emdbId: new FormControl(""),
-      orcid: this.fb.array([]),
+      orcid: this.fb.array([
+        this.fb.group({
+          orcidId: ['', [Validators.required, Validators.pattern(/^(\d{4}-){3}\d{4}$/)]],
+        })
+      ]),
     })
   }
-
-  get orcidArray(): FormArray {
-    return this.form.get('orcid') as FormArray;
-  }
-
   hasUnsavedChanges() {
     return this._hasUnsavedChanges;
   }
   onHasUnsavedChanges($event: boolean) {
     this._hasUnsavedChanges = $event;
   }
-  addOrcidField(): void {
+  orcidArray(): FormArray {
+    return this.form.get('orcid') as FormArray;
+  }
+  addOrcidField() {
     const orcidField = this.fb.group({
       orcidId: ['', [Validators.required, Validators.pattern(/^(\d{4}-){3}\d{4}$/)]],
     });
-    this.orcidArray.push(orcidField);
+    this.orcidArray().push(orcidField);
   }
-  removeOrcidField(index: number): void {
-    this.orcidArray.removeAt(index);
+  removeOrcidField(index: number) {
+    if (this.orcidArray().length > 1) {
+      this.orcidArray().removeAt(index);
+    }
   }
-
-  onMethodChange(): void {
-    this.fileTypes = this.methodsList.find(mL => mL.value=== this.form.value['emMethod']).files;
-    this.fileTypes[this.emFile.MainMap].required = true;
-    this.fileTypes[this.emFile.Image].required = true;
-    switch (this.form.value['emMethod']){
+  onMethodChange() {
+    this.fileTypes = this.methodsList.find(mL => mL.value === this.form.value['emMethod']).files;
+    this.fileTypes.forEach((fT) => {
+      if (fT.emName === this.emFile.MainMap || fT.emName === this.emFile.Image) {
+        fT.required = true;
+      } else {
+        fT.required = false;
+      }
+    });
+    switch (this.form.value['emMethod']) {
       case 'helical':
-      this.fileTypes[this.emFile.HalfMap1].required = true;
-      this.fileTypes[this.emFile.HalfMap2].required = true;
+        this.fileTypes.forEach((fT) => {
+          if (fT.emName === this.emFile.HalfMap1 || fT.emName === this.emFile.HalfMap2) {
+            fT.required = true;
+          }
+        });
         break;
       case "single-particle":
-        this.fileTypes[this.emFile.HalfMap1].required = true;
-        this.fileTypes[this.emFile.HalfMap2].required = true;
-        this.fileTypes[this.emFile.MaskMap].required = true;
+        this.fileTypes.forEach((fT) => {
+          if (fT.emName === this.emFile.HalfMap1 || fT.emName === this.emFile.HalfMap2 || fT.emName === this.emFile.MaskMap) {
+            fT.required = true;
+          }
+        });
         break;
     }
-    const sortedDepositionSet = Object.entries(this.fileTypes)
-        .sort(([keyA, valueA], [keyB, valueB]) => {
-            if (valueA.required && !valueB.required) return -1; // valueA comes first
-            if (!valueA.required && valueB.required) return 1;  // valueB comes first
-            return 0; 
-        })
-        .reduce((sortedObj, [key, value]) => {
-            sortedObj[key as EmFile] = value;
-            return sortedObj;
-        }, {} as { [f in EmFile]: DepositionFiles });
-
-    this.fileTypes = sortedDepositionSet;
-
   }
-  onPDB(event: any): void {
-    const input = event.value;  
+
+  onPDB(event: any) {
+    const input = event.value;
     if (input === 'true') {
-      this.fileTypes[this.emFile.Coordinates].required = true;
-    } 
+      this.fileTypes.forEach((fT) => {
+        if (fT.emName === this.emFile.Coordinates) {
+          fT.required = true;
+        }
+      });
+    }
   }
 
-  autoGrow(event: Event): void {
+  autoGrow(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
     const lineHeight = parseInt(getComputedStyle(textarea).lineHeight, 10);
     const maxLines = 5;
@@ -160,90 +163,211 @@ export class OneDepComponent implements OnInit {
     // Update overflow property based on height
     this.detailsOverflow = textarea.scrollHeight > newHeight ? 'auto' : 'hidden';
   }
-
-  get fileTypesEntries() {
-    return Object.entries(this.fileTypes || {}).map(([key, value]) => ({ key, value }));
-  }
-
-  onChooseFile(fileInput: HTMLInputElement): void {
+  onChooseFile(fileInput: HTMLInputElement) {
     fileInput.click();
   }
-  onFileSelected(event: Event, controlName: string) {
+  onFileSelected(event: Event, controlName: EmFile) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.selectedFile[controlName] = input.files[0];
-      this.fileTypes[controlName].file = this.selectedFile[controlName];
-      this.fileTypes[controlName].fileName = this.selectedFile[controlName].name;
+      this.fileTypes.forEach((fT) => {
+        if (fT.emName === controlName) {
+          fT.file = this.selectedFile[controlName];
+          fT.fileName = this.selectedFile[controlName].name;
+        }
+      });
     }
+  }
+  onFileAddMapSelected(event: Event, id: number) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      // Use the ID to store the file uniquely for each "add-map"
+      this.selectedFile[`add-map-${id}`] = input.files[0];
+      this.fileTypes.forEach((fT) => {
+        if (fT.emName === this.emFile.AddMap && fT.id === id) {
+          fT.file = this.selectedFile[`add-map-${id}`];
+          fT.fileName = this.selectedFile[`add-map-${id}`].name;
+        }
+      });
+    }
+  }
+  isRequired(controlName: string): boolean {
+    let value: boolean;
+    this.fileTypes.forEach((fT) => {
+      if (fT.emName === controlName) {
+        value = fT.required;
+      }
+    });
+    return value;
   }
   updateContourLevelMain(event: Event) {
     const input = (event.target as HTMLInputElement).value.trim();
     const normalizedInput = input.replace(',', '.');
     const parsedValue = parseFloat(normalizedInput);
     if (!isNaN(parsedValue)) {
-      [EmFile.MainMap, EmFile.HalfMap1, EmFile.HalfMap2].forEach((key) => {
-        if (this.fileTypes[key]) {
-          this.fileTypes[key].contour = parsedValue;
+      this.fileTypes.forEach((fT) => {
+        if (fT.emName === EmFile.MainMap || fT.emName === EmFile.HalfMap1 || fT.emName === EmFile.HalfMap2) {
+          fT.contour = parsedValue;
         }
       });
     } else {
       console.warn('Invalid number format:', input);
     }
   }
-  updateContourLevel(event: Event, controlName: string) {
+  updateContourLevelAddMap(event: Event, id: number) {
     const input = (event.target as HTMLInputElement).value.trim();
     const normalizedInput = input.replace(',', '.');
     const parsedValue = parseFloat(normalizedInput);
     if (!isNaN(parsedValue)) {
-      this.fileTypes[controlName].contour = parsedValue;
+      this.fileTypes.forEach((fT) => {
+        if (fT.emName === this.emFile.AddMap && fT.id === id) {
+          fT.contour = parsedValue;
+        }
+      });
     } else {
       console.warn('Invalid number format:', input);
     }
   }
-  updateDetails(event: Event, controlName: string) {
+  updateContourLevel(event: Event, controlName: EmFile) {
+    const input = (event.target as HTMLInputElement).value.trim();
+    const normalizedInput = input.replace(',', '.');
+    const parsedValue = parseFloat(normalizedInput);
+    if (!isNaN(parsedValue)) {
+      this.fileTypes.forEach((fT) => {
+        if (fT.emName === controlName) {
+          fT.contour = parsedValue;
+        }
+      });
+    } else {
+      console.warn('Invalid number format:', input);
+    }
+  }
+  updateDetails(event: Event, controlName: EmFile) {
     const textarea = event.target as HTMLTextAreaElement; // Cast to HTMLTextAreaElement
     const value = textarea.value;
-    if (this.fileTypes[controlName]) {
-      this.fileTypes[controlName].details = value;
-    }
-  }
- 
-  isFormValid(): boolean {
-    // Ensure all required files in EmFiles have been selected
-    return Object.keys(this.fileTypes).every((fileKey) => {
-        const file = this.fileTypes[fileKey as EmFile];
-        return !file.required || !!this.fileTypes[file.type]?.file;  // If required, ensure it's selected
+    this.fileTypes.forEach((fT) => {
+      if (fT.emName === controlName) {
+        fT.details = value;
+      }
     });
-}
-  isRequired(key: string): boolean {
-    return this.fileTypes[key].required;
+
+  }
+  updateDetailsAddMap(event: Event, id: number) {
+    const textarea = event.target as HTMLTextAreaElement; // Cast to HTMLTextAreaElement
+    const value = textarea.value;
+    this.fileTypes.forEach((fT) => {
+      if (fT.emName === this.emFile.AddMap && fT.id === id) {
+        fT.details = value;
+      }
+    });
+
+  }
+  addMap() {
+    const nextId = this.fileTypes
+      .filter(file => file.emName === EmFile.AddMap)
+      .reduce((maxId, file) => (file.id > maxId ? file.id : maxId), 0) + 1;
+
+    const newMap: DepositionFiles = {
+      emName: EmFile.AddMap,
+      id: nextId,
+      nameFE: 'Additional Map ( ' + (nextId + 1).toString() + ' )',
+      type: "add-map",
+      fileName: "",
+      file: null,
+      contour: 0.0,
+      details: "",
+      required: false,
+    };
+
+    this.fileTypes.push(newMap);
+  }
+  sendFile(depID: string, form: FormData) {
+    this.http.post("http://localhost:8080/onedep/" + depID + "/file", form).subscribe({
+      next: (res) => console.log('Uploaded', this.emFile[form.get("fileMetadata")["type"]],res),
+      error: (error) => console.error('Could not upload File and Metadata', error),
+    });
+  }
+  sendCoordFile(depID: string, form: FormData) {
+    this.http.post("http://localhost:8080/onedep/" + depID + "/pdb", form).subscribe({
+      next: (res) => console.log('Uploaded Coordinates and Metadata', res),
+      error: (error) => console.error('Could not upload Coordinates and Metadata', error),
+    });
+  }
+  sendMetadata(depID: string, body: string) {
+    // missing token!
+    this.http.post("http://localhost:8080/onedep/" + depID + "/metadata", body, {
+      headers: { 'Content-Type': 'application/json' },
+    }).subscribe({
+      next: (res) => console.log('Uploaded Metadata', res),
+      error: (error) => console.error('Could not upload Metadata', error),
+    });
   }
   onDepositClick() {
-    const formDataToSend = new FormData();
-    formDataToSend.append('email', this.form.value.email);
-    formDataToSend.append('orcidIds', this.form.value.orcidArray);
-    formDataToSend.append('metadata', JSON.stringify(this.form.value.metadata));
-    formDataToSend.append('experiments', this.form.value.emMethod);
-    // emdbId: this.form.value.emdbId, 
-    var fileMetadata = []
-
-    for (const key in this.fileTypes) {
-      if (this.fileTypes[key].file) {
-        formDataToSend.append('file', this.fileTypes[key].file);
-        fileMetadata.push({ name: this.fileTypes[key].name, type: this.fileTypes[key].type, contour: this.fileTypes[key].contour, details: this.fileTypes[key].details });
-      }
-    }
-    formDataToSend.append('fileMetadata', JSON.stringify(fileMetadata));
-    this.http.post("http://localhost:8080/onedep", formDataToSend, {
-      headers: {}
-    }).subscribe(
-      response => {
-        console.log('Created deposition in OneDep', response);
-      },
-      error => {
-        console.error('Request failed', error.error);
+    //  Create a deposition
+    const body = JSON.stringify(
+      {
+        "email": "sofya.laskina@epfl.ch",  // for now
+        "orcidIds": this.orcidArray().value.map(item => item.orcidId),
+        "country": "United States",
+        "method": this.form.value.emMethod,
+        "jwtToken": this.form.value.jwtToken,
       }
     );
+    let depID: string;
+    let metadataAdded = false;
+    depID = "D_800043";
+    // this.http.post("http://localhost:8080/onedep", body, {
+    //  headers: { 'Content-Type': 'application/json' },
+    // }).subscribe({
+    //   next: (response: any) => {
+    //     depID = response.depID; // Update the outer variable
+    //     console.log('Created deposition in OneDep', depID);
+
+    //     // Call subsequent requests
+    this.fileTypes.forEach((fT) => { 
+      if (fT.file) {
+        const formDataFile = new FormData()
+        formDataFile.append('jwtToken', this.form.value.jwtToken)
+        formDataFile.append('file', fT.file);
+        formDataFile.append('fileMetadata', JSON.stringify({ name: fT.fileName, type: fT.type, contour: fT.contour, details: fT.details }))
+        if (fT.emName === this.emFile.Coordinates) {
+          formDataFile.append('metadata', JSON.stringify(this.form.value.metadata));
+          this.sendCoordFile(depID, formDataFile);
+          metadataAdded = true
+        } else {
+          this.sendFile(depID, formDataFile);
+        }
+      }
+    });
+    if (! metadataAdded){
+      const metadataBody = JSON.stringify({
+        metadata: this.form.value.metadata,
+        jwtToken: this.form.value.jwtToken ,
+      });
+      this.sendMetadata(depID, metadataBody);
+    }
+    //   },
+    //   error: (error) => console.error('Request failed', error.error),
+    // });
+
+    // const formDataFile = new FormData();
+    // formDataToSend.append('jwtToken', this.form.value.jwtToken);
+    // 
+
+    // var fileMetadata = []
+    // // for (const fI in this.fileTypes) {
+    // 
+    // formDataToSend.append('fileMetadata', JSON.stringify(fileMetadata));
+    // this.http.post("http://localhost:8080/onedep", formDataToSend, {
+    //   headers: {}
+    // }).subscribe(
+    //   response => {
+    //     console.log('Created deposition in OneDep', response);
+    //   },
+    //   error => {
+    //     console.error('Request failed', error.error);
+    //   }
+    // );
 
   }
 }
--- src/app/datasets/onedep/types/methods.enum.ts
+++ src/app/datasets/onedep/types/methods.enum.ts
@@ -19,36 +19,11 @@ export enum EmFile {
     StructureFactors = "xs-cif",
     MTZ = "xs-mtz",
 };
-enum BasicDeposition {
-    MainMap = 'vo-map',
-    MaskMap = 'mask-map',
-    AddMap = 'add-map',
-    Image = 'img-emdb',
-    FSC = 'fsc-xml',
-    LayerLines = "layer-lines",
-}
-enum ExtendedDeposition {
-    HalfMap1 = 'half-map1',
-    HalfMap2 = 'half-map2',
-    Coordinates = 'co-cif',
-}
-enum CristallographyDeposition{
-    StructureFactors = "xs-cif",
-    MTZ = "xs-mtz",
-}
-export interface OneDepExperiment {
-    type: string;
-    subtype?: string;
-}
 
-interface EmMethod {
-    value: EmType;
-    viewValue: string;
-    experiment: OneDepExperiment;
-    files: { [key in EmFile]?: DepositionFiles };
-}
 
 export interface DepositionFiles {
+    emName: EmFile;
+    id?: number, 
     nameFE: string;
     type: string,
     fileName: string,
@@ -58,150 +33,192 @@ export interface DepositionFiles {
     required: boolean,
 }
 
-const BasicDepositionSet: { [f in BasicDeposition]: DepositionFiles } = {
-    [EmFile.Image]: {
-        nameFE: 'Public Image',
-        type: "img-emdb",
-        fileName: "",
-        file: null,
-        details: "",
-        required: false,
-    },
-    // add metadata later
-    [EmFile.MainMap]: {
-        nameFE: 'Main Map',
-        type: "vo-map",
-        fileName: "",
-        file: null,
-        contour: 0.0,
-        details: "",
-        required: false,
-    },
-    [EmFile.MaskMap]: {
-        nameFE: 'Mask Map',
-        type: "mask-map",
-        fileName: "",
-        file: null,
-        contour: 0.0,
-        details: "",
-        required: false,
-    },
-    [EmFile.AddMap]: {
-        nameFE: 'Additional Map',
-        type: "add-map",
-        fileName: "",
-        file: null,
-        contour: 0.0,
-        details: "",
-        required: false,
-    },
-    [EmFile.FSC]: {
-        nameFE: 'FSC-XML',
-        type: "fsc-xml",
-        fileName: "",
-        file: null,
-        details: "",
-        required: false,
-    },
-    [EmFile.LayerLines]: {
-        nameFE: 'Other: Layer Lines Data ',
-        type: "layer-lines",
-        fileName: "",
-        file: null,
-        details: "",
-        required: false,
-    },
-};
-
-const ExtendedDepositionSet: { [f in ExtendedDeposition]: DepositionFiles } = {
-    [EmFile.HalfMap1]: {
-        nameFE: 'Half Map (1)',
-        type: "half-map",
-        fileName: "",
-        file: null,
-        contour: 0.0,
-        details: "",
-        required: false,
-    },
-    [EmFile.HalfMap2]: {
-        nameFE: 'Half Map (2)',
-        type: "half-map",
-        fileName: "",
-        file: null,
-        contour: 0.0,
-        details: "",
-        required: false,
-    },
-    [EmFile.Coordinates]: {
-        nameFE: 'Coordinates',
-        type: "co-cif",
-        fileName: "",
-        file: null,
-        details: "",
-        required: false,
-    }
+interface EmMethod {
+    value: EmType;
+    viewValue: string;
+    files:  DepositionFiles[];
 }
 
-const CristallographyDepositionSet: { [f in CristallographyDeposition]: DepositionFiles } = {
+export const DepositionImage: DepositionFiles ={
+    emName: EmFile.Image,
+    nameFE: 'Public Image',
+    type: "img-emdb",
+    fileName: "",
+    file: null,
+    details: "",
+    required: false,
+}
+export const DepositionMainMap:DepositionFiles ={
+    emName: EmFile.MainMap,
+    nameFE: 'Main Map',
+    type: "vo-map",
+    fileName: "",
+    file: null,
+    contour: 0.0,
+    details: "",
+    required: false,
+}
+export const DepositionHalfMap1:DepositionFiles ={
+    emName: EmFile.HalfMap1,
+    nameFE: 'Half Map (1)',
+    type: "half-map",
+    fileName: "",
+    file: null,
+    contour: 0.0,
+    details: "",
+    required: false,
+}
+export const DepositionHalfMap2:DepositionFiles ={
+    emName: EmFile.HalfMap2,
+    nameFE: 'Half Map (2)',
+    type: "half-map",
+    fileName: "",
+    file: null,
+    contour: 0.0,
+    details: "",
+    required: false,
+}
+export const DepositionMaskMap:DepositionFiles ={
+    emName: EmFile.MaskMap,
+    nameFE: 'Mask Map',
+    type: "mask-map",
+    fileName: "",
+    file: null,
+    contour: 0.0,
+    details: "",
+    required: false,
+}
+export const DepositionAddMap:DepositionFiles ={
+    emName: EmFile.AddMap,
+    id:0,
+    nameFE: 'Additional Map',
+    type: "add-map",
+    fileName: "",
+    file: null,
+    contour: 0.0,
+    details: "",
+    required: false,
+}
+export const DepositionFSC:DepositionFiles ={
+    emName: EmFile.FSC,
+    nameFE: 'FSC-XML',
+    type: "fsc-xml",
+    fileName: "",
+    file: null,
+    details: "",
+    required: false,
+}
+export const DepositionLayerLines:DepositionFiles ={
+    emName: EmFile.LayerLines,
+    nameFE: 'Other: Layer Lines Data ',
+    type: "layer-lines",
+    fileName: "",
+    file: null,
+    details: "",
+    required: false,
+}
+export const DepositionCoordinates: DepositionFiles = {
+    emName: EmFile.Coordinates,
+    nameFE: 'Coordinates',
+    type: "co-cif",
+    fileName: "",
+    file: null,
+    details: "",
+    required: false,
+}
+export const DepositionStructureFactors: DepositionFiles = {
+    emName: EmFile.StructureFactors,
+    nameFE: 'Structure Factors',
+    type: "xs-cif",
+    fileName: "",
+    file: null,
+    details: "",
+    required: false,
+}
+export const DepositionMTZ: DepositionFiles = {
+    emName: EmFile.MTZ,
+    nameFE: 'MTZ',
+    type: "xs-mtz",
+    fileName: "",
+    file: null,
+    details: "",
+    required: false,
+}
 
-        [EmFile.StructureFactors]:{
-            nameFE: 'Structure Factors',
-            type: "xs-cif",
-            fileName: "",
-            file: null,
-            details: "",
-            required: false,
-        },
-        [EmFile.MTZ]:{
-            nameFE: 'MTZ',
-            type: "xs-mtz",
-            fileName: "",
-            file: null,
-            details: "",
-            required: false,
-        },
-    }
 
 export const MethodsList: EmMethod[] = [
     {
         value: EmType.Helical,
         viewValue: 'Helical',
-        experiment: { type: "em", subtype: "helical" },
-        files: {
-            ...BasicDepositionSet, ...ExtendedDepositionSet,
-        },
+        files: [
+            DepositionImage, 
+            DepositionCoordinates, 
+            DepositionMainMap, 
+            DepositionMaskMap, 
+            DepositionHalfMap1, 
+            DepositionHalfMap2, 
+            DepositionFSC,
+            DepositionLayerLines,
+            DepositionAddMap, 
+        ],
     },
     {
         value: EmType.SingleParticle,
         viewValue: 'Single Particle',
-        experiment: { type: "em", subtype: "single" },
-        files: {
-            ...BasicDepositionSet, ...ExtendedDepositionSet,
-        },
+        files: [
+            DepositionImage, 
+            DepositionCoordinates, 
+            DepositionMainMap, 
+            DepositionMaskMap, 
+            DepositionHalfMap1, 
+            DepositionHalfMap2, 
+            DepositionFSC,
+            DepositionLayerLines,
+            DepositionAddMap, 
+        ],
     },
     {
         value: EmType.SubtomogramAveraging,
         viewValue: 'Subtomogram Averaging',
-        experiment: { type: "em", subtype: "subtomogram" },
-        files: {
-            ...BasicDepositionSet, ...ExtendedDepositionSet,
-        },
+        files:[
+            DepositionImage, 
+            DepositionCoordinates, 
+            DepositionMainMap, 
+            DepositionMaskMap, 
+            DepositionHalfMap1, 
+            DepositionHalfMap2, 
+            DepositionFSC,
+            DepositionLayerLines,
+            DepositionAddMap, 
+        ],
     },
     {
         value: EmType.Tomogram,
         viewValue: 'Tomogram',
-        experiment: { type: "em", subtype: "tomography" },
-        files: BasicDepositionSet,
+        files: [
+            DepositionImage, 
+            DepositionMainMap, 
+            DepositionMaskMap, 
+            DepositionFSC,
+            DepositionLayerLines,
+            DepositionAddMap, 
+        ],
     },
     {
         value: EmType.ElectronCristallography,
         viewValue: 'Electron Crystallography',
-        experiment: { type: "ec" },
-        files: {
-            ...BasicDepositionSet,
-            ...ExtendedDepositionSet,
-            ...CristallographyDepositionSet,
-        }
+        files: [
+            DepositionImage, 
+            DepositionCoordinates, 
+            DepositionMainMap, 
+            DepositionMaskMap, 
+            DepositionHalfMap1, 
+            DepositionHalfMap2, 
+            DepositionStructureFactors,
+            DepositionMTZ,
+            DepositionFSC,
+            DepositionLayerLines,
+            DepositionAddMap, 
+        ],
     },
 ];
-
